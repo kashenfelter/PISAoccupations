a30423fca6b7f3c5e7cbab78c19002732d249a23
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-14 18:38:17 +0200

    project reorganised, function for calculating standard errors added

---
+++ R/mean_ppvs.R
@@ -0,0 +1,28 @@
+#' Mean calculated separately for each of the five plausible values and a given weight.
+#'
+#' @param pvname String of the form "MATH", "READ", "SCIE".
+#' @param groups Name of one or more factors used for grouping.
+#' @param final_weight Name of a column that contains final student weights.
+#' @param data Name of a data frame containing columns given in previous arguments.
+#'
+#' @return Data frame containing columns for each of given factor variables, five columns with
+#'         means calculated for each plausible values and a column with sums of weights.
+
+mean_ppvs <- function(pvname, groups, weights, data) {
+    pvlabs <- paste0(paste0("PV", 1:5), pvname)
+    data %>%
+        group_by_(.dots = groups) %>%
+        select_(.dots = c(pvlabs, weights)) %>%
+        mutate_(wpv1 = interp(~p*w, p = as.name(pvlabs[1]), w = as.name(weights)),
+                wpv2 = interp(~p*w, p = as.name(pvlabs[2]), w = as.name(weights)),
+                wpv3 = interp(~p*w, p = as.name(pvlabs[3]), w = as.name(weights)),
+                wpv4 = interp(~p*w, p = as.name(pvlabs[4]), w = as.name(weights)),
+                wpv5 = interp(~p*w, p = as.name(pvlabs[5]), w = as.name(weights)),
+                weights = interp(~w, w = as.name(weights))) %>%
+        summarise(mpv1 = sum(wpv1)/sum(weights),
+                  mpv2 = sum(wpv2)/sum(weights),
+                  mpv3 = sum(wpv3)/sum(weights),
+                  mpv4 = sum(wpv4)/sum(weights),
+                  mpv5 = sum(wpv5)/sum(weights),
+                  population.share = sum(weights))
+}
---
+++ R/mean_pv.R
@@ -0,0 +1,17 @@
+#' Average student's performances with standard errors by given factor variables.
+#'
+#' @param pvname pvname String of the form "MATH", "READ", "SCIE".
+#' @param groups Name of one or more factors used for grouping.
+#' @param final_weight Name of a column that contains final student weights.
+#' @param brr_weights Names of the columns that contain BRR weights.
+#' @param data Name of a data frame containing columns given in previous arguments.
+#'
+#' @return Data frame with columns for each given factor and student's average performances with standard errors.
+#'
+#' @export
+
+mean_pv <- function(pvname, groups, final_weight, brr_weights, data) {
+    tmp <- mean_ppvs(pvname, groups, final_weight, data)
+    tmp %>% select(-starts_with("mpv")) -> tmp2
+    return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weight, brr_weights, tmp, data)))
+}
