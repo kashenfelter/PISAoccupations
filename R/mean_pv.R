34bf992fece6fc4d20917971ebe5fcfcca496e56
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-15 15:16:43 +0200

    mean_pv can now return just means or means and SEs.

--- R/mean_pv.R
+++ R/mean_pv.R
@@ -1,17 +1,22 @@
 #' Average student's performances with standard errors by given factor variables.
 #'
-#' @param pvname pvname String of the form "MATH", "READ", "SCIE".
+#' @param pvname String of the form "MATH", "READ", "SCIE".
 #' @param groups Name of one or more factors used for grouping.
 #' @param final_weight Name of a column that contains final student weights.
-#' @param brr_weights Names of the columns that contain BRR weights.
 #' @param data Name of a data frame containing columns given in previous arguments.
+#' @param se TRUE if standard error are to be returned, FALSE if only means are to be returned.
+#' @param brr_weights Names of the columns that contain BRR weights. If se is set to FALSE,
+#'                    this argument is redunant and thus the default value is "".
 #'
 #' @return Data frame with columns for each given factor and student's average performances with standard errors.
 #'
 #' @export
 
-mean_pv <- function(pvname, groups, final_weight, brr_weights, data) {
+mean_pv <- function(pvname, groups, final_weight, data, se = T, brr_weights = "") {
     tmp <- mean_ppvs(pvname, groups, final_weight, data)
     tmp %>% select(-starts_with("mpv")) -> tmp2
-    return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weight, brr_weights, tmp, data)))
+    if(se)
+        return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weight, brr_weights, tmp, data)))
+    else
+        return(data.frame(tmp2, "mean" = mean_o(tmp)))
 }
