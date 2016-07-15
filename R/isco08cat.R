1610af67dd5ecde6f0258b4cf48fdfda4aec5fc0
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-15 10:50:37 +0200

    isco categories extraction

---
+++ R/isco08cat.R
@@ -0,0 +1,15 @@
+#' Extract first and first two digits of ISCO08 code as factors both for mother's and father's occupation.
+#'
+#' @param isco_cols Names of columns with mother's and father's occupation codes (in this order).
+#' @param data Name of a data frame containing columns given in previous arguments.
+#'
+#' @return Data frame given in 'data' argument with four described columns added.
+#'
+
+isco08_cat <- function(isco_cols, data) {
+    data %>%
+        mutate_(mother_occuI = interp("as.factor(substr(i, 1, 1))", i = as.name(isco_cols[1])),
+                mother_occuII = interp("as.factor(substr(i, 1, 2))", i = as.name(isco_cols[1])),
+                father_occuI = interp("as.factor(substr(i, 1, 1))", i = as.name(isco_cols[2])),
+                father_occuII = interp("as.factor(substr(i, 1, 2))", i = as.name(isco_cols[2])))
+}
