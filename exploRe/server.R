25b37282aca9219b5b1fcf4429dfb7144ac7ff99
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-19 13:37:10 +0200

    error bars

--- exploRe/server.R
+++ exploRe/server.R
@@ -1,12 +1,12 @@
-# library(shiny)
 # library(dplyr)
-library(ggplot2)
+# library(ggplot2)
 # library(lazyeval)
-library(ggrepel)
+# library(ggrepel)
 # library(devtools)
 # library(PISAoccupations)
 # load("data/pisa.rda")
 # load("data/isco_text_plt.rda")
+
 shinyServer(function(input, output) {
      output$rainbow2 <- renderPlot(
           plot_rainbow_shiny(input$subject, c(input$cnt1, input$cnt2), input$years[1])
