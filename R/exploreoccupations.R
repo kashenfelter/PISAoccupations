dfa3d00966985d454d6488787c1e688b7000ff20
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-22 14:47:53 +0200

    function that starts the app added

---
+++ R/exploreoccupations.R
@@ -0,0 +1,7 @@
+#' Rainbow plot of average performances for two countries grouped by isco categories designed for shiny app.
+#'
+#' @export
+
+exploreOccupations <- function() {
+    runApp("exploRe")
+}
