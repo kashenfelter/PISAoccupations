ca1094fab5ed4fa4396718fe39175ba8772ad2a2
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-23 23:34:48 +0200

    rainbow plot fix

--- R/plotrainbowshiny.R
+++ R/plotrainbowshiny.R
@@ -11,12 +11,12 @@
 plot_rainbow_shiny <- function(csubject, cnts, cyear) {
     pisa %>%
         filter(subject == csubject, year == cyear, cnt %in% cnts) %>%
-        arrange(as.character(cnt), as.numeric(as.character(isco))) %>%
+        arrange(as.character(cnt), s.character(isco)) %>%
         mutate(no = if_else(cnt == sort(cnts)[1], 1, 5)) %>%
-        mutate(isco2 = ifelse(!(isco == "cnt"),
-                              isco_text_plt[as.integer(as.character(isco))+1], as.character(cnt))) -> sdf
-    # Powyższa linijka wymaga poprawy.
-    ggplot(sdf, aes(x = no, y = ave.perf, color = isco, group = isco2, label = isco2)) +
+        mutate(isco2 = ifelse(isco == "cnt", as.character(cnt), "tba")) -> sdf
+    sdf$isco2[sdf$isco != "cnt"] <- isco_text_plt[as.integer(as.character(sdf$isco[sdf$isco != "cnt"])) + 1]
+
+        ggplot(sdf, aes(x = no, y = ave.perf, color = isco, group = isco2, label = isco2)) +
         theme_bw() +
         geom_line(size = 1.5) +
         geom_text_repel(data = subset(sdf, no == 1 & !(isco2 %in% cnts)), nudge_x = -1) +
