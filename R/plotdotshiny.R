bc2b31d70f8c769f494d666d2f6cf59a9d74b534
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-27 12:20:30 +0200

    argument added

--- R/plotdotshiny.R
+++ R/plotdotshiny.R
@@ -2,12 +2,13 @@
 #'
 #' @param csubject Character of the from "MATH"/"READ"/"SCIE".
 #' @param cyear Chosen years  - as a character - 2003/2006/2009/2012.
+#' @param isco_cats ISCO categories to display.
 #'
 #' @return GGplot2 object.
 #'
 #' @export
 
-plot_dot_shiny <- function(csubject, cyear) {
+plot_dot_shiny <- function(csubject, cyear, isco_cats) {
     pisa %>%
         filter(subject == csubject,
                year == cyear) %>%
@@ -16,10 +17,11 @@ plot_dot_shiny <- function(csubject, cyear) {
                          {function(x) return(names(country_names)[grep(country_names, pattern = x)])})
 
     ggplot(subset(sdf, isco == "cnt"), aes(x = reorder(cnt_lab, ave.perf), y = ave.perf)) +
-        geom_point(data = subset(sdf, isco %in% c(as.character(1:4), "cnt")), aes(size = pop.share, color = isco)) +
+        geom_point(data = subset(sdf, isco %in% c(isco_cats, "cnt")), aes(size = pop.share, color = isco)) +
         coord_flip() +
         theme_bw() +
         xlab("") +
         ylab("") +
-        scale_size_continuous(guide = F)
+        scale_size_continuous(guide = F) +
+        scale_color_discrete(name = "Category")
 }
