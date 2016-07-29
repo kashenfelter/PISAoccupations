b2b32205d72cd95afb75c6d6ddb5e0587e26bde1
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-07-29 14:17:11 +0200

    little update

--- R/plotrainbow.R
+++ R/plotrainbow.R
@@ -10,9 +10,10 @@
 
 plot_rainbow <- function(csubject, cnts, cyear) {
     pisa %>%
-        filter(subject == csubject, year == cyear, cnt %in% cnts) %>%
-        arrange(as.character(cnt), as.character(isco)) %>%
-        mutate(no = if_else(cnt == cnts[1], 1, 3),
+        filter(subject == csubject,
+               year == cyear,
+               cnt %in% cnts) %>%
+        mutate(no = ifelse(cnt == cnts[1], 1, 3),
                isco2 = ifelse(isco == "cnt", cnt_lab, isco_lab)) -> sdf
 
     ggplot(subset(sdf, isco != "cnt"), aes(x = no, y = ave.perf, color = isco, group = isco2, label = isco2)) +
