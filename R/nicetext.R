255c7bddfff9b1b9d57e6a9827eb71900674a8bf
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-08-01 15:59:00 +0200

    home page update, nice_text fix

--- R/nicetext.R
+++ R/nicetext.R
@@ -14,6 +14,7 @@ nice_text <- function(ave_perfs) {
                 next
             else {
                 ave_perfs[i] <- ave_perfs[i] + 1
+                ave_perfs[i - 1] <- ave_perfs[i - 1] - 1
                 nc <- nc + 1
             } # else
         } # for
