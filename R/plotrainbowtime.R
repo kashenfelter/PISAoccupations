1423fbe44996e13dffd30945f0eadf5e2c8099ae
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-08-03 17:45:40 +0200

    lines fixed

--- R/plotrainbowtime.R
+++ R/plotrainbowtime.R
@@ -9,7 +9,8 @@
 plot_rainbow_time <- function(sdf) {
     ggvis(sdf, x = ~year, y = ~ave.perf) %>%
         group_by(isco) %>%
-        layer_paths(stroke = ~isco) %>%
+        arrange(year) %>%
+        layer_paths(stroke := ~color) %>%
         layer_points(fill := ~color) %>%
         layer_points(fill := ~color, key := ~id) %>% # To przez jakiś bug w ggvis.
         hide_legend("fill") %>%
