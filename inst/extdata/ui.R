69aceb230e816eb82adf2f4f609386e81d37d1b1
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-08-04 15:05:27 +0200

    google analytics

--- inst/extdata/ui.R
+++ inst/extdata/ui.R
@@ -1,5 +1,11 @@
 shinyUI(fluidPage(
-  tags$head(tags$style(
+  tags$head(tags$script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                        ga('create', 'UA-5650686-6', 'auto');
+                        ga('send', 'pageview');"),
+  tags$style(
     HTML("#col1 #col2 #col3 #col4 { background-color: #f8f8f8; }
          #time #rainbow2{ height: 100vh !important; }
          #title1 #title2 { text-align: center;
