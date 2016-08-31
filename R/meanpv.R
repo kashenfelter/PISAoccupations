3d83b5c73d3188858cd463f4b83da1b7c1e58e9a
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-08-31 02:13:37 +0200

    file structured changed

--- R/meanpv.R
+++ R/meanpv.R
@@ -1,19 +1,72 @@
-#' Average student's performances with standard errors by given factor variables.
+#' Average performance for one plausible value and a given replicate weight.
 #'
 #' @param data Name of a data frame containing columns given in other arguments.
-#' @param pvname String of the form "MATH"/"READ"/"SCIE".
+#' @param pvname Name of the plausible value column.
 #' @param groups Name of one or more factors used for grouping.
-#' @param final_weight Name of a column that contains final student weights.
+#' @param final_weights Name of a column that contains final student weights.
+#'
+#' @return Data frame containing columns for each of given factor variables, five columns with
+#'         means calculated for each plausible values and a column with sums of weights.
+
+mean_pvse <- function(data, pvname, groups, final_weights) {
+    data %>%
+        select_(.dots = c(pvname, groups,  final_weights)) %>%
+        group_by_(.dots = groups) %>%
+        rename_(.dots = setNames(c(pvname, final_weights), c("PV", "W_F"))) %>%
+        summarise(mpv1 = sum(PV*W_F, na.rm = T)/sum(W_F, na.rm = T))
+}
+
+
+#' Mean calculated separately for each of the five plausible values and a given weight.
+#'
+#' @inheritParams mean_pvse
+#' @param school_id Name of a column with school IDs.
+#'
+#' @return Data frame containing columns for each of given factor variables, five columns with
+#'         means calculated for each plausible values and a column with sums of weights.
+
+mean_ppvs <- function(data, pvname, groups, final_weights = "W_FSTUWT", school_id = "SCH_ID") {
+    pvlabs <- paste0(paste0("PV", 1:5), pvname)
+    data %>%
+        select_(.dots = c(pvlabs, groups, final_weights, school_id)) %>%
+        group_by_(.dots = groups) %>%
+        rename_(.dots = setNames(c(final_weights, pvlabs, school_id), c("W_F", paste0("PV", 1:5), "SCH_ID"))) %>%
+        summarise(mpv1 = sum(PV1*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
+                  mpv2 = sum(PV2*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
+                  mpv3 = sum(PV3*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
+                  mpv4 = sum(PV4*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
+                  mpv5 = sum(PV5*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
+                  population.share = sum(W_F, na.rm = TRUE),
+                  nstud = n(),
+                  nschool = n_distinct(SCH_ID, na.rm = TRUE))
+}
+
+
+#' Mean calculated from five plausible values.
+#'
+#' @param means_ppv Data frame returned by mean_ppvs function that contains average performances computed
+#'                  for each plausible value.
+#'
+#' @return Vector of student's average performances.
+#'
+
+mean_o <- function(means_ppv) {
+    return(rowSums(means_ppv[, paste0("mpv", 1:5)], na.rm  = T)/5)
+}
+
+
+#' Average student's performances with standard errors by given factor variables.
+#'
+#' @inheritParams mean_ppvs
 #' @param brr_weights Names of the columns that contain BRR weights. If se is set to FALSE,
 #'                    this argument is redunant and thus the default value is "".
-#' @param school_id Name of a column with school ids.
 #'
 #' @return Data frame with columns for each given factor and student's average performances with standard errors.
 #'
 #' @export
 
-mean_pv <- function(data, pvname, groups, final_weight, brr_weights, school_id) {
-    tmp <- mean_ppvs(pvname, groups, final_weight, school_id, data)
+mean_pv <- function(data, pvname, groups, final_weights, brr_weights, school_id) {
+    tmp <- mean_ppvs(pvname, groups, final_weights, school_id, data)
     tmp %>% select(-starts_with("mpv")) -> tmp2
-    return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weight, brr_weights, tmp, data)))
+    return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weights, brr_weights, tmp, data)))
 }
--- R/sepv.R
+++ R/sepv.R
@@ -1,12 +1,53 @@
-#' Standard errors for average performances grouped by given factors.
+#' Sampling variance for one plausible values and a set of replicate weights.
+#'
+#' @inheritParams var_sml
+#' @param final_weight Name of a column with student's final weight.
+#'
+#' @return Vector that contains computed sampling variances.
+#'
+
+var_sml_opv <- function(data, pvname, groups, final_weight = "W_FSTUWT", brr_weights = paste0("W_FSTR", 1:80)) {
+    replicate_means <- lapply(brr_weights, {function(x) return(mean_pvse(pvname, groups, x, data)[, "mpv1"])})
+    diffs <- lapply(data.frame(replicate_means), {function(x) return((x - mean_pvse(pvname, groups, final_weight, data)[, "mpv1"])^2)})
+    return(0.05*rowSums(data.frame(diffs), na.rm = T))
+}
+
+
+#' Sampling variance for a given set of plausible values and weights.
 #'
 #' @param data Name of a data frame containing columns given in other arguments.
 #' @param pvname String of the form "MATH", "READ", "SCIE".
 #' @param groups Name of one or more factors used for grouping.
+#' @param brr_weights Names of columns that contain BRR weights.
+#'
+#' @return Vector that contains computed sampling variances.
+#'
+
+var_sml <- function(data, pvname, groups, final_weight = "W_FSTUWT", brr_weights = paste0("W_FSTR", 1:80)) {
+    pvlabs <- paste0(paste0("PV", 1:5), pvname)
+    varsmls <- lapply((1:5), {function(x) return(var_sml_opv(pvlabs[x], groups, brr_weights,final_weight, data))})
+    return(0.2*rowSums(data.frame(varsmls), na.rm = T))
+}
+
+
+#' Imputation variance for given set of plausible values and weights.
+#'
 #' @param means_ppv Data frame returned by mean_ppvs function that contains average performances
 #'        computed for each plausible value.
-#' @param final_weight Name of a column that contains final student weights.
-#' @param brr_weights Names of the columns that contain BRR weights.
+#' @return Vector containing computed imputation variances.
+#'
+
+var_imp <- function(means_ppv) {
+    return(0.25*rowSums(data.frame(lapply(means_ppv[, paste0("mpv", 1:5)],
+                                          {function(x) return((x - mean_o(means_ppv))^2)})), na.rm = T))
+}
+
+
+#' Standard errors for average performances grouped by given factors.
+#'
+#' @inheritParams var_sml_opv
+#' @param means_ppv Data frame returned by mean_ppvs function that contains average performances
+#'        computed for each plausible value.
 #'
 #' @return Vector that contains computed standard errors.
 #'
