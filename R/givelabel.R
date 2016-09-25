0af435a2a6b0f463269a403b60d7a6917e08919a
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-09-25 02:02:22 +0200

    avoid repeating "<br />"

--- R/givelabel.R
+++ R/givelabel.R
@@ -5,12 +5,13 @@
 #' @return html object for a tooltip.
 #'
 
-give_label <- function(point) {
+giveLabel <- function(point) {
     src <- as.list(pisa[pisa$id == point$id, ])
-    return(paste("Subject:", src$subject, "<br />",
-          src$cnt_lab, "<br />",
-          src$isco_lab, "<br />",
-          "Mean (plausible values):", round(src$ave.perf, 2), "<br />",
-          "Standard error:", round(src$se, 2), "<br />",
-          "Population share:", paste0(round(src$pop.share/1000), "k")))
+    return(paste(paste("Subject:", src$subject), 
+		 src$cnt_lab, 
+		 src$isco_lab,
+		 paste("Mean (plausible values):", round(src$ave.perf, 2)), 
+		 paste("Standard error:", round(src$se, 2)), 
+		 paste("Population share:", paste0(round(src$pop.share/1000), "k")),
+		 sep = "<br />")
 }
