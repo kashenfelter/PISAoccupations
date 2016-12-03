67aae0decd34af8d0806b4745a10f3bc9ad0ee9f
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-12-03 20:48:09 +0100

    additional argument, counting school and students fixed

--- R/meanpv.R
+++ R/meanpv.R
@@ -20,24 +20,26 @@ mean_pvse <- function(data, pvname, groups, final_weights) {
 #' Mean calculated separately for each of the five plausible values and a given weight.
 #'
 #' @inheritParams mean_pvse
+#' @param student_id Name of a column with student IDs.
 #' @param school_id Name of a column with school IDs.
 #'
 #' @return Data frame containing columns for each of given factor variables, five columns with
 #'         means calculated for each plausible values and a column with sums of weights.
 
-mean_ppvs <- function(data, pvname, groups, final_weights = "W_FSTUWT", school_id = "SCH_ID") {
+mean_ppvs <- function(data, pvname, groups, final_weights, student_id, school_id) {
     pvlabs <- paste0(paste0("PV", 1:5), pvname)
     data %>%
         select_(.dots = c(pvlabs, groups, final_weights, school_id)) %>%
         group_by_(.dots = groups) %>%
-        rename_(.dots = setNames(c(final_weights, pvlabs, school_id), c("W_F", paste0("PV", 1:5), "SCH_ID"))) %>%
+        rename_(.dots = setNames(c(final_weights, pvlabs, student_id, school_id), 
+				 c("W_F", paste0("PV", 1:5), "STU_ID", "SCH_ID"))) %>%
         summarise(mpv1 = sum(PV1*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
                   mpv2 = sum(PV2*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
                   mpv3 = sum(PV3*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
                   mpv4 = sum(PV4*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
                   mpv5 = sum(PV5*W_F, na.rm = TRUE)/sum(W_F, na.rm = TRUE),
-                  population.share = sum(W_F, na.rm = TRUE),
-                  nstud = n(),
+                  population.share = 0.5*sum(W_F, na.rm = TRUE),
+                  nstud = n_distinct(STUD_IDi, na.rm = TRUE),
                   nschool = n_distinct(SCH_ID, na.rm = TRUE))
 }
 
@@ -51,13 +53,14 @@ mean_ppvs <- function(data, pvname, groups, final_weights = "W_FSTUWT", school_i
 #'
 
 mean_o <- function(means_ppv) {
-    return(rowSums(means_ppv[, paste0("mpv", 1:5)], na.rm  = T)/5)
+  0.2*rowSums(means_ppv[, paste0("mpv", 1:5)], na.rm  = T)
 }
 
 
 #' Average student's performances with standard errors by given factor variables.
 #'
 #' @inheritParams mean_ppvs
+#' @param student_id Name of a column with student IDs.
 #' @param brr_weights Names of the columns that contain BRR weights. If se is set to FALSE,
 #'                    this argument is redunant and thus the default value is "".
 #'
@@ -65,8 +68,8 @@ mean_o <- function(means_ppv) {
 #'
 #' @export
 
-mean_pv <- function(data, pvname, groups, final_weights, brr_weights, school_id) {
-    tmp <- mean_ppvs(pvname, groups, final_weights, school_id, data)
-    tmp %>% select(-starts_with("mpv")) -> tmp2
-    return(data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weights, brr_weights, tmp, data)))
+mean_pv <- function(data, pvname, groups, final_weights, brr_weights, student_id, school_id) {
+  tmp <- mean_ppvs(data, pvname, groups, final_weights, student_id, school_id)
+  tmp %>% select(-starts_with("mpv")) -> tmp2
+  data.frame(tmp2, "mean" = mean_o(tmp), "se" = se_pv(pvname, groups, final_weights, brr_weights, tmp, data))
 }
