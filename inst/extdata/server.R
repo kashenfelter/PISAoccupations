f89056b168e53b6b54b213116de823434dcf31e6
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-12-06 00:07:09 +0100

    ggiraph outputs and plots' sizes

--- inst/extdata/server.R
+++ inst/extdata/server.R
@@ -1,16 +1,18 @@
 shinyServer(function(input, output) {
-    output$rainbow <- renderPlot(
-      plotRainbow(input$subject1, c(input$cnt12, input$cnt22), input$cyear1),
-      height = 800
+    output$rainbow <- renderggiraph(
+      ggiraph(code = print(plotRainbow(input$subject1, c(input$cnt12, input$cnt22), input$cyear1)),
+	      width = 1)
     )
-    output$time <- renderPlot(
-      plotTime(input$subject, c(input$cnt1, input$cnt2), c(input$se, input$trend), input$isco_cats),
-      height = 800
-    )
+    output$time <- renderggiraph(
+      ggiraph(code = print(plotTime(input$subject, c(input$cnt1, input$cnt2), c(input$se, input$trend), input$isco_cats)),
+	      width = 1)
+      )
     output$rainbowTime <- renderggiraph(
-      ggiraph(code = print(plotRainbowTime(input$subjectt1, c(input$cnt11, input$cnt21))))
+      ggiraph(code = print(plotRainbowTime(input$subjectt1, c(input$cnt11, input$cnt21))),
+	      width = 1)
     )
     output$dots <- renderggiraph(
-      ggiraph(code = print(plotDot(input$subjectt, input$cyear, c("cnt", input$isco_cats2))))
+      ggiraph(code = print(plotDot(input$subjectt, input$cyear, c("cnt", input$isco_cats2))),
+	      width = 1)
     )
 })
