498ccc9c3cde61d99ea97f3ed63a2e08d0dda2c0
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-12-05 13:13:31 +0100

    slight change

--- R/plotrainbow.R
+++ R/plotrainbow.R
@@ -19,7 +19,7 @@ nice_text <- function(ave_perfs) {
             } # else
         } # for
     } # while
-    return(ave_perfs)
+    ave_perfs
 }
 
 
@@ -47,6 +47,7 @@ plotRainbow <- function(chosenSubject, countries, chosenYear) {
                    cnt %in% countries) %>%
             mutate(no = ifelse(cnt == countries[1], 1, 3),
                    isco2 = ifelse(isco == "cnt", cnt_lab, isco_lab),
+		   label = giveLabel(subject, cnt_lab, isco_lab, ave.perf, se, pop.share),
                    text_pos = ave.perf) %>%
             arrange(cnt, ave.perf)-> sdf
         sdf$text_pos[sdf$no == 1] <- nice_text(sdf$text_pos[sdf$no == 1])
@@ -57,12 +58,12 @@ plotRainbow <- function(chosenSubject, countries, chosenYear) {
 
         ggplot(subset(sdf, isco != "cnt"), aes(x = no, y = ave.perf, color = isco, group = isco2, label = isco2)) +
             theme_tufte(base_size = 16) +
-            geom_point(data = subset(sdf, isco != "cnt"), aes(size = pop.share)) +
+            geom_point_interactive(data = subset(sdf, isco != "cnt"), aes(size = pop.share, tooltip = label)) +
             geom_line(size = 1.5) +
             geom_text(data = subset(sdf, isco != "cnt" & no == 1), aes(y = text_pos), hjust = "outward", size = 7, nudge_x = -0.1) +
             geom_text(data = subset(sdf, isco != "cnt" & no == 3), aes(y = text_pos), hjust = "outward", size = 7, nudge_x = 0.1) +
             geom_line(data = subset(sdf, isco == "cnt"), aes(group = year), size = 1.5, color = "black") +
-            geom_point(data = subset(sdf, isco == "cnt"), color = "black") +
+            geom_point_interactive(data = subset(sdf, isco == "cnt"), aes(tooltip = label), color = "black") +
             geom_text(data = subset(sdf, isco == "cnt" & no == 1),
                       color = "black", hjust = "outward", size = 7, nudge_x = -0.1) +
             geom_text(data = subset(sdf, isco == "cnt" & no == 3),
