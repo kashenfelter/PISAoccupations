f06e395535d91dc7894164e1ce6a0a9ff8f78bfe
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-12-06 00:08:14 +0100

    interactivity added, sizes changed

--- R/plottime.R
+++ R/plottime.R
@@ -19,15 +19,15 @@ plotTime <- function(csubject, cnts, disp, isco_cats = as.character(1:9)) {
         mutate(label = giveLabel(subject, cnt_lab, isco_lab,
 				 ave.perf, se, pop.share)) -> sdf
     names(vals) <- unique(c(sdf$cnt_lab[sdf$cnt == cnts[1]], sdf$cnt_lab[sdf$cnt == cnts[2]]))
+
     ggplot(sdf, aes(x = year, y = ave.perf, shape = cnt_lab,
                     color = isco, group = as.factor(paste0(isco, cnt))), linetype = 2) +
-        geom_point(size = 4, stroke = 2) +
-        theme_tufte(base_size = 16) +
+        geom_point_interactive(aes(tooltip = label), size = 1.5, stroke = 2) +
+        theme_tufte(base_size = 8) +
         theme(legend.position = c(0.94, 0.92),
               axis.text.x = element_text(angle = 90),
               panel.grid.major.y = element_line(linetype = 2, size = 0.5, color = "grey"),
-              panel.grid = element_line(linetype = 2, size = 0.5, color = "grey"),
-              axis.text = element_text(face = "bold")) +
+              panel.grid = element_line(linetype = 2, size = 0.5, color = "grey")) +
         scale_shape_manual(name = "Country", values = vals) +
         scale_color_manual(values = colors, guide = "none") +
         xlab("Year of study") +
@@ -35,11 +35,12 @@ plotTime <- function(csubject, cnts, disp, isco_cats = as.character(1:9)) {
         facet_grid(~isco, labeller = as_labeller(naming)) -> plt
 
     if(disp[1])
-        plt <- plt + geom_pointrange((aes(ymin = ave.perf - se, ymax = ave.perf + se)), linetype = 2)
+        plt <- plt + geom_pointrange((aes(ymin = ave.perf - se, ymax = ave.perf + se)), linetype = 2) +
+        geom_point_interactive(aes(tooltip = label), size = 1.5, stroke = 2)
     if(disp[2])
         plt <- plt + geom_line(data = subset(sdf, cnt == cnts[1]), stat = "smooth",
-                  linetype = 1, method = "lm", se = F, size = 1.5, show.legend = F) +
+                  linetype = 1, method = "lm", se = F, size = 1, show.legend = F) +
         geom_line(data = subset(sdf, cnt == cnts[2]), stat = "smooth",
-                  linetype = 2, alpha = 0.5, method = "lm", se = F, size = 1.5, show.legend = F)
-    return(plt)
+                  linetype = 2, alpha = 0.5, method = "lm", se = F, size = 1, show.legend = F)
+    plt
 }
