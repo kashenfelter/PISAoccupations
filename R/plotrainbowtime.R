cbdde53334cf17c48c3e8248888e24ef4d6fb58d
mstaniak <mateusz.staniak@math.uni.wroc.pl>
2016-12-06 00:08:01 +0100

    sizes

--- R/plotrainbowtime.R
+++ R/plotrainbowtime.R
@@ -15,9 +15,9 @@ plotRainbowTime <- function(csubject, cnts) {
 			   ave.perf, se, pop.share)) -> sdf
 
   ggplot(sdf, aes(x = year, y = ave.perf, color = isco, group = isco_lab)) +
-    geom_line(size = 1.5) + 
-    geom_point_interactive(aes(tooltip = label), size = 3) +
-    theme_tufte(base_size = 16) +
+    geom_line(size = 1) + 
+    geom_point_interactive(aes(tooltip = label), size = 1.5) +
+    theme_tufte(base_size = 10) +
     theme(panel.grid.major.y = element_line(linetype = 2, size = 0.5, color = "grey"),
 	  panel.grid = element_line(linetype = 2, size = 0.5, color = "grey")) +
     scale_color_manual(values = colors, guide = "none") +
