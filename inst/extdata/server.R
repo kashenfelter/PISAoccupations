25b7548e7c09cd13ec7041e6e93f616dbc1039d5
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-01-10 00:38:48 +0100

    trochę lepsze proporcje na wykresach?

--- inst/extdata/server.R
+++ inst/extdata/server.R
@@ -1,18 +1,18 @@
 shinyServer(function(input, output) {
     output$rainbow <- renderggiraph(
       ggiraph(code = print(plotRainbow(input$subject1, c(input$cnt12, input$cnt22), input$cyear1)),
-	      width = 1)
+         width = 1, height_svg=6, width_svg=9)
     )
     output$time <- renderggiraph(
       ggiraph(code = print(plotTime(input$subject, c(input$cnt1, input$cnt2), c(input$se, input$trend), input$isco_cats)),
-	      width = 1)
+       width = 1, height_svg=5, width_svg=8)
       )
     output$rainbowTime <- renderggiraph(
       ggiraph(code = print(plotRainbowTime(input$subjectt1, c(input$cnt11, input$cnt21))),
-	      width = 1)
-    )
-    output$dots <- renderggiraph(
+       width =  0.8, height_svg=7, width_svg=9)                      
+    )                                                           
+    output$dots <- renderggiraph(                               
       ggiraph(code = print(plotDot(input$subjectt, input$cyear, c("cnt", input$isco_cats2))),
-	      width = 1)
-    )
-})
+       width = 1,  height_svg=6, width_svg=9.5)                    
+    )                                                           
+})                                                              
